9b59d840a79f6236f6bbd0895c205d103fe75dd9
zeeshan-rsys <67018643+Zeeshan-progs@users.noreply.github.com>
2023-03-05 20:11:40 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,32 @@ TextStyle textStyle({double? fontSize, Color? color, FontWeight? fontWeight}) =>
     );
 
 class MyApp extends StatefulWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      theme: ThemeData.dark(),
+      builder: (context, child) => const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   DateTime now = DateTime.now();
   Timer? timer;
   @override
@@ -35,7 +54,7 @@ class _MyAppState extends State<MyApp> {
       timer = Timer.periodic(const Duration(seconds: 1), (t) {
         setState(() {
           // to update new time with new time after adding a sec in current time
-          now = now.add(Duration(seconds: 1));
+          now = now.add(const Duration(seconds: 1));
         });
       });
     });
@@ -49,99 +68,101 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
-      theme: ThemeData.dark(),
-      builder: (context, child) => Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Cloack animation',
-            style: textStyle(
-              color: Colors.white,
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Cloack animation',
+          style: textStyle(
+            color: Colors.white,
           ),
         ),
-        body: SafeArea(
-            child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              TimeWidget(
-                time: DateFormat('HH').format(now),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TimeWidget(
-                time: DateFormat('mm').format(now),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TimeWidget(
-                time: DateFormat('ss').format(now),
-              ),
-            ],
-          ),
-        )),
       ),
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(
+              height: 60,
+            ),
+            TimeWidget(
+              time: DateFormat('HH').format(now),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TimeWidget(
+              time: DateFormat('mm').format(now),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TimeWidget(
+              time: DateFormat('ss').format(now),
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
 
 class TimeWidget extends StatelessWidget {
-  const TimeWidget({
+  TimeWidget({
     Key? key,
     required this.time,
   }) : super(key: key);
 
   final String time;
-
+  EdgeInsets padding = const EdgeInsets.all(8);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      width: 150,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(17),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.white.withOpacity(0.03),
-            offset: const Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Stack(
-        children: [
-          // lets add some simple animation component
-          Center(
-            child: AnimatedSwitcherWidget(time: time),
-          ),
-          Center(
-            child: Row(
-              children: [
-                Container(
-                  height: 20,
-                  width: 5,
-                  color: Colors.black54,
-                ),
-                const Expanded(
-                  child: Divider(
-                    thickness: 3,
+    final size = MediaQuery.of(context).size;
+    return Align(
+      alignment: Alignment.center,
+      child: Container(
+        height: 150,
+        width: 150,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(17),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.white.withOpacity(0.03),
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Stack(
+          children: [
+            // lets add some simple animation component
+            Center(
+              child: AnimatedSwitcherWidget(time: time),
+            ),
+            Center(
+              child: Row(
+                children: [
+                  Container(
+                    height: 20,
+                    width: 5,
                     color: Colors.black54,
                   ),
-                ),
-                Container(
-                  height: 20,
-                  width: 5,
-                  color: Colors.black54,
-                ),
-              ],
+                  const Expanded(
+                    child: Divider(
+                      thickness: 3,
+                      color: Colors.black54,
+                    ),
+                  ),
+                  Container(
+                    height: 20,
+                    width: 5,
+                    color: Colors.black54,
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
